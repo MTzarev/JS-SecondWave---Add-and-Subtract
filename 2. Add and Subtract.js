13cd9c89b88655027635248bf4fcedfbb57b1de6
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-31 23:14:58 +0300

    Update 2. Add and Subtract.js

--- 2. Add and Subtract.js
+++ 2. Add and Subtract.js
@@ -2,7 +2,6 @@ function addAndSubtract(num1, num2, num3) {
     let a = num1;
     let b = num2;
     let c = num3;
-
     function sum(a, b) {
         return a + b;
     }
@@ -11,7 +10,6 @@ function addAndSubtract(num1, num2, num3) {
         return sumRes - c;
     }
     console.log(subtract(a, b, c));
-
 }
 addAndSubtract(23,
     6,
